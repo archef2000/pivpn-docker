25eb4820a82859150649231819e5ae02d5e945ab
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-01-06 22:59:53 +0100

    Update setupVars.sh

--- setupVars.sh
+++ setupVars.sh
@@ -1,2 +1,5 @@
 SetupVars=/etc/pivpn/setupVars.conf
-sed -i "s/pivpnNET=/pivpnNET=$PIVPNNET/" "${SetupVars}"
+sed -i "s/pivpnNET=10.8.0.0/pivpnNET=$PIVPNNET/" "${SetupVars}"
+sed -i "s/VPN=openvpn/VPN=$VPN/" "${SetupVars}"
+sed -i "s/TWO_POINT_FOUR=1/TWO_POINT_FOUR="$2.4"/" "${SetupVars}"
+
