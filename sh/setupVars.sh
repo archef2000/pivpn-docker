ef3bb348449577aefcd48d6354375cc6d22a6d79
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-01-09 19:06:51 +0100

    Update setupVars.sh

--- sh/setupVars.sh
+++ sh/setupVars.sh
@@ -2,6 +2,12 @@
 
 IPV4DNS=${IPV4DNS:=172.17.0.1}
 
+if [ $TWO_POINT_FOUR = "1" ]; then
+   ENCRYPTION=256
+else
+  ENCRYPTION=2048
+fi
+
 cat << EOF > /etc/pivpn/setupVars.conf
 ENCRYPT=256
 USING_UFW=0
@@ -20,7 +26,7 @@ pivpnDNS2=${DNS2:=8.8.4.4}
 pivpnHOST=${HOST:=example.com}
 pivpnSEARCHDOMAIN=${SEARCHDOMAIN:=}
 TWO_POINT_FOUR=${TWO_POINT_FOUR:=1}
-pivpnENCRYPT=${ENCRYPT:=2048} # 2048, 3072, or 4096
+pivpnENCRYPT=${ENCRYPT:="$ENCRYPTION"} # 2048, 3072, or 4096
 USE_PREDEFINED_DH_PARAM=${PREDEFINED_DH_PARAM:=1}
 pivpnNET=${NET:=10.8.0.0}
 subnetClass=${subnetClass:=24}
