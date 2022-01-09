cf7caffcfc14f3334b8c1631f3333509b476be1c
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-01-09 13:57:46 +0100

    Update setupVars.sh

--- setupVars.sh
+++ setupVars.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cat << EOF > /etc/pivpn/setupVars.conf
+
 USING_UFW=0
 IPv4dev=eth0
 pivpnInterface=eth0
@@ -9,15 +10,15 @@ install_home=/home/pivpn
 VPN=openvpn
 pivpnDEV=tun0
 UNATTUPG=1
-pivpnPROTO="${PROTO:=udp}"
-pivpnPORT="${PORT:=1194}"
-pivpnDNS1="${DNS1:=8.8.8.8}"
-pivpnDNS2="${DNS2:=8.8.4.4}"
-pivpnHOST="${HOST:=pivpn}"
-pivpnSEARCHDOMAIN="${SEARCHDOMAIN:=}"
-TWO_POINT_FOUR="${TWO_POINT_FOUR:=1}"
-pivpnENCRYPT="${ENCRYPT:=256}"
-USE_PREDEFINED_DH_PARAM="${PREDEFINED_DH_PARAM:=1}"
-pivpnNET="${NET:=10.8.0.0}"
-subnetClass="${subnetClass:=24}"
+pivpnPROTO=${PROTO:=udp}
+pivpnPORT=${PORT:=1194}
+pivpnDNS1=${DNS1:=8.8.8.8}
+pivpnDNS2=${DNS2:=8.8.4.4}
+pivpnHOST=${HOST:=pivpn}
+pivpnSEARCHDOMAIN=${SEARCHDOMAIN:=}
+TWO_POINT_FOUR=${TWO_POINT_FOUR:=1}
+pivpnENCRYPT=${ENCRYPT:=256}
+USE_PREDEFINED_DH_PARAM=${PREDEFINED_DH_PARAM:=1}
+pivpnNET=${NET:=10.8.0.0}
+subnetClass=${subnetClass:=24}
 EOF
