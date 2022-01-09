b8e0c63927a1363e0cad5fa3a0b72cf4fe384206
Archef2000 <69057532+Archef2000@users.noreply.github.com>
2022-01-09 15:05:35 +0100

    Update reconfigure.sh

--- sh/reconfigure.sh
+++ sh/reconfigure.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-Writung setupVars.conf
-bash /home/pivpn/setupVars.sh
+echo "Writung setupVars.conf"
+bash /etc/pivpn/setupVars.sh
 
-Reconfigure PIVPN
+echo "Reconfigure PIVPN"
 INSTALLER=/etc/pivpn/install.sh
 curl -fsSL0 https://install.pivpn.io -o "${INSTALLER}" \
     && sed -i '/setStaticIPv4 #/d' "${INSTALLER}" \
     && chmod +x "${INSTALLER}" \
     && "${INSTALLER}" --unattended /etc/pivpn/setupVars.conf --reconfigure
+
+pivpn -a nopass -n "${CLIENT_NAME}" -d 1080 || true
